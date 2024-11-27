f0cdf616ef3e59fa33e5de38c21f54f9b0fefb44
junitaazizah <mikozua45@gmail.com>
2024-11-27 22:28:09 +0700

    test

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ isset($title) ? $title : 'LARAVEL' }}</title>
-    <link rel="icon" href="{{ asset('img/laravel.svg') }}" />
+    <link rel="icon" href="{{ asset('img/laravel.jpg') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMF2b1Z5Z4F+3U1T5jF5N0z5h6h5F4Qf8D4Zf4" crossorigin="anonymous">
     <style>
